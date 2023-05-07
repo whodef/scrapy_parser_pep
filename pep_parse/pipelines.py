01d9a2376629779bee7b13a4bcac5828192b1e44
whodef <tatyana@selyuk.com>
2023-05-07 23:50:27 +0300

    feat: Final changes

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -5,32 +5,34 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent
+FIELDS_NAME = ('Статус', 'Количество')
+DATE_FORMAT = '%Y-%m-%dT%H-%M-%S'
+TIME_NOW = dt.datetime.now().strftime(DATE_FORMAT)
+
+
+def write_results(status_summary):
+    timestamp = TIME_NOW
+    results_dir = BASE_DIR / 'results'
+    results_dir.mkdir(exist_ok=True)
+    file_name = f'status_summary_{timestamp}.csv'
+    file_path = results_dir / file_name
+    result = (
+        [FIELDS_NAME] + status_summary.most_common() +
+        [('Total', sum(status_summary.values()))]
+    )
+
+    with open(file_path, 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, dialect='unix')
+        writer.writerows(result)
 
 
 class PepParsePipeline:
-    def __init__(self):
-        self.status_summary = Counter()
-
     def open_spider(self, spider):
-        pass
+        self.status_summary = Counter()
 
     def process_item(self, item, spider):
         self.status_summary[item['status']] += 1
         return item
 
-    @staticmethod
-    def close_spider(self, status_summary, spider):
-        now_formatted = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        results_dir = BASE_DIR / 'results'
-        results_dir.mkdir(exist_ok=True)
-        file_name = f'status_summary_{now_formatted}.csv'
-        file_path = results_dir / file_name
-        result = (
-            [('Статус', 'Количество')]
-            + status_summary.most_common() +
-            [('Total', sum(status_summary.values()))]
-        )
-
-        with open(file_path, 'w', encoding='utf-8') as f:
-            writer = csv.writer(f, dialect='unix')
-            writer.writerows(result)
+    def close_spider(self, spider):
+        write_results(self.status_summary)
